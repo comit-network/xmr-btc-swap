295216a8eed7dbaf1670fd4aacfd3ee32dbee66c
Philipp Hoenisch <philipp@hoenisch.at>
2020-10-20 14:33:32 +1100

    Add Tor feature flag and only run test if enabled

--- xmr-btc/tests/tor.rs
+++ xmr-btc/tests/tor.rs
@@ -1,64 +1,69 @@
-use anyhow::Result;
-use hyper::service::{make_service_fn, service_fn};
-use reqwest::StatusCode;
-use spectral::prelude::*;
-use std::convert::Infallible;
-use tokio::sync::oneshot::Receiver;
-use torut::onion::TorSecretKeyV3;
-use xmr_btc::tor::{AuthenticatedConnection, TOR_PROXY_ADDR};
+#[cfg(feature = "tor")]
+mod tor_test {
 
-async fn hello_world(
-    _req: hyper::Request<hyper::Body>,
-) -> Result<hyper::Response<hyper::Body>, Infallible> {
-    Ok(hyper::Response::new("Hello World".into()))
-}
+    use anyhow::Result;
+    use hyper::service::{make_service_fn, service_fn};
+    use reqwest::StatusCode;
+    use spectral::prelude::*;
+    use std::convert::Infallible;
+    use tokio::sync::oneshot::Receiver;
+    use torut::onion::TorSecretKeyV3;
+    use xmr_btc::tor::{AuthenticatedConnection, TOR_PROXY_ADDR};
 
-fn start_test_service(port: u16, rx: Receiver<()>) {
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
-    let addr = ([127, 0, 0, 1], port).into();
-    let server = hyper::Server::bind(&addr).serve(make_svc);
-    let graceful = server.with_graceful_shutdown(async {
-        rx.await.ok();
-    });
-    tokio::spawn(async {
-        // server.await.unwrap();
-        if let Err(e) = graceful.await {
-            eprintln!("server error: {}", e);
-        }
-    });
-}
+    async fn hello_world(
+        _req: hyper::Request<hyper::Body>,
+    ) -> Result<hyper::Response<hyper::Body>, Infallible> {
+        Ok(hyper::Response::new("Hello World".into()))
+    }
 
-#[tokio::test]
-async fn test_tor_control_port() -> Result<()> {
-    // Setup test HTTP Server
-    let (tx, rx) = tokio::sync::oneshot::channel::<()>();
-    let port = 8080;
-    start_test_service(port, rx);
+    fn start_test_service(port: u16, rx: Receiver<()>) {
+        let make_svc =
+            make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
+        let addr = ([127, 0, 0, 1], port).into();
+        let server = hyper::Server::bind(&addr).serve(make_svc);
+        let graceful = server.with_graceful_shutdown(async {
+            rx.await.ok();
+        });
+        tokio::spawn(async {
+            // server.await.unwrap();
+            if let Err(e) = graceful.await {
+                eprintln!("server error: {}", e);
+            }
+        });
+    }
 
-    // Connect to local Tor service
-    let mut authenticated_connection = AuthenticatedConnection::new().await?;
+    #[tokio::test]
+    async fn test_tor_control_port() -> Result<()> {
+        // Setup test HTTP Server
+        let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+        let port = 8080;
+        start_test_service(port, rx);
 
-    // Expose an onion service that re-directs to the echo server.
-    let tor_secret_key_v3 = TorSecretKeyV3::generate();
-    authenticated_connection
-        .add_service(port, &tor_secret_key_v3)
-        .await?;
+        // Connect to local Tor service
+        let mut authenticated_connection = AuthenticatedConnection::new().await?;
 
-    // Test if Tor service forwards to HTTP Server
+        // Expose an onion service that re-directs to the echo server.
+        let tor_secret_key_v3 = TorSecretKeyV3::generate();
+        authenticated_connection
+            .add_service(port, &tor_secret_key_v3)
+            .await?;
 
-    let proxy = reqwest::Proxy::all(format!("socks5h://{}", *TOR_PROXY_ADDR).as_str())
-        .expect("tor proxy should be there");
-    let client = reqwest::Client::builder().proxy(proxy).build().unwrap();
-    let onion_address = tor_secret_key_v3.public().get_onion_address().to_string();
-    let onion_url = format!("http://{}:8080", onion_address);
+        // Test if Tor service forwards to HTTP Server
 
-    let res = client.get(&onion_url).send().await?;
-    assert_that(&res.status()).is_equal_to(StatusCode::OK);
+        let proxy = reqwest::Proxy::all(format!("socks5h://{}", *TOR_PROXY_ADDR).as_str())
+            .expect("tor proxy should be there");
+        let client = reqwest::Client::builder().proxy(proxy).build().unwrap();
+        let onion_address = tor_secret_key_v3.public().get_onion_address().to_string();
+        let onion_url = format!("http://{}:8080", onion_address);
 
-    let text = res.text().await?;
-    assert_that!(text).contains("Hello World");
+        let res = client.get(&onion_url).send().await?;
+        assert_that(&res.status()).is_equal_to(StatusCode::OK);
 
-    // gracefully shut down server
-    let _ = tx.send(());
-    Ok(())
+        let text = res.text().await?;
+        assert_that!(text).contains("Hello World");
+
+        // gracefully shut down server
+        let _ = tx.send(());
+        Ok(())
+    }
 }
