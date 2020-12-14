6ff440d0ccf7161aaffbe2fb4d7879246eaf8d6d
rishflab <rishflab@hotmail.com>
2020-12-14 11:28:52 +1100

    Remove unused function

--- xmr-btc/src/bob.rs
+++ xmr-btc/src/bob.rs
@@ -621,48 +621,6 @@ impl State3 {
         })
     }
 
-    pub async fn refund_btc<W: bitcoin::BroadcastSignedTransaction>(
-        &self,
-        bitcoin_wallet: &W,
-    ) -> Result<()> {
-        let tx_cancel =
-            bitcoin::TxCancel::new(&self.tx_lock, self.refund_timelock, self.A, self.b.public());
-        let tx_refund = bitcoin::TxRefund::new(&tx_cancel, &self.refund_address);
-
-        {
-            let sig_b = self.b.sign(tx_cancel.digest());
-            let sig_a = self.tx_cancel_sig_a.clone();
-
-            let signed_tx_cancel = tx_cancel.clone().add_signatures(
-                &self.tx_lock,
-                (self.A, sig_a),
-                (self.b.public(), sig_b),
-            )?;
-
-            let _ = bitcoin_wallet
-                .broadcast_signed_transaction(signed_tx_cancel)
-                .await?;
-        }
-
-        {
-            let adaptor = Adaptor::<Sha256, Deterministic<Sha256>>::default();
-
-            let sig_b = self.b.sign(tx_refund.digest());
-            let sig_a = adaptor
-                .decrypt_signature(&self.s_b.into_secp256k1(), self.tx_refund_encsig.clone());
-
-            let signed_tx_refund = tx_refund.add_signatures(
-                &tx_cancel.clone(),
-                (self.A, sig_a),
-                (self.b.public(), sig_b),
-            )?;
-
-            let _ = bitcoin_wallet
-                .broadcast_signed_transaction(signed_tx_refund)
-                .await?;
-        }
-        Ok(())
-    }
     pub fn tx_lock_id(&self) -> bitcoin::Txid {
         self.tx_lock.txid()
     }
