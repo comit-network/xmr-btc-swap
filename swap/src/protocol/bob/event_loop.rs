faceaa5312ff3e3c50409e24bb5990022bec2950
Thomas Eizinger <thomas@eizinger.io>
2021-03-31 22:26:06 +1100

    Only log connections to Alice but those on info level

--- swap/src/protocol/bob/event_loop.rs
+++ swap/src/protocol/bob/event_loop.rs
@@ -134,10 +134,7 @@ impl EventLoop {
                             return;
                         }
                         SwarmEvent::ConnectionEstablished { peer_id, endpoint, .. } if peer_id == self.alice_peer_id => {
-                            tracing::debug!("Connected to Alice at {}", endpoint.get_remote_address());
-                        }
-                        SwarmEvent::ConnectionEstablished { peer_id, endpoint, .. } => {
-                            tracing::debug!("Connected to {} at {}", peer_id, endpoint.get_remote_address());
+                            tracing::info!("Connected to Alice at {}", endpoint.get_remote_address());
                         }
                         SwarmEvent::Dialing(peer_id) if peer_id == self.alice_peer_id => {
                             tracing::debug!("Dialling Alice at {}", peer_id);
