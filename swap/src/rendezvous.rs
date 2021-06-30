01bfa93cf7536acead76d9a54cda4074df284390
Daniel Karzel <daniel@comit.network>
2021-06-30 18:27:39 +1000

    testnet is not mainnet

--- swap/src/rendezvous.rs
+++ swap/src/rendezvous.rs
@@ -10,7 +10,7 @@ impl Display for XmrBtcNamespace {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             XmrBtcNamespace::Mainnet => write!(f, "xmr-btc-swap-mainnet"),
-            XmrBtcNamespace::Testnet => write!(f, "xmr-btc-swap-mainnet"),
+            XmrBtcNamespace::Testnet => write!(f, "xmr-btc-swap-testnet"),
         }
     }
 }
