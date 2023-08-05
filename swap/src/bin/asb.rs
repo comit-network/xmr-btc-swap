f0c5883b50e8a0df3effcb8004f6695393343d66
pokkst <pokkst@protonmail.com>
2023-08-05 13:04:28 -0500

    Fix more descriptive log message

--- swap/src/bin/asb.rs
+++ swap/src/bin/asb.rs
@@ -117,8 +117,11 @@ async fn main() -> Result<()> {
     };
     let tor_port = if _ac.is_some() { config.tor.socks5_port } else { 0u16 };
     let proxy_string = if tor_port != 0u16 { format!("127.0.0.1:{}", tor_port) } else { "".to_string() };
-    let info_message = if proxy_string.is_empty() { "Not using SOCKS5 proxy" } else { "Using SOCKS5 proxy at" };
-    tracing::info!(%proxy_string, info_message);
+    if proxy_string.is_empty() {
+        tracing::info!(%proxy_string, "Not using SOCKS5 proxy");
+    } else {
+        tracing::info!(%proxy_string, "Using SOCKS5 proxy at");
+    }
 
     match cmd {
         Command::Start { resume_only } => {
