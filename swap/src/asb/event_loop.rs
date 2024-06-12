3549f9d49a9d6b09976df5320bc8cf6c39ebc29d
patrini32 <171664803+patrini32@users.noreply.github.com>
2024-06-12 21:50:43 +0300

    Revert "Fix lint and remove fetch state3 from db in Alice's event_loop"
    This reverts commit 6abf46b6b7d3fb779a5ac8c8efd790de497d271c.

--- swap/src/asb/event_loop.rs
+++ swap/src/asb/event_loop.rs
@@ -283,14 +283,25 @@ where
                                 continue;
                             };
 
-                            let state3 = match state {
-                                State::Alice (AliceState::BtcPunished { state3 }) => state3,
-                                _ => {
+                            let State::Alice (AliceState::BtcPunished { .. }) = state else {
                                 tracing::warn!(%swap_id, "Ignoring cooperative XMR redeem request for swap in invalid state");
                                 continue;
-                                }
                             };
-                            if self.swarm.behaviour_mut().cooperative_xmr_redeem.send_response(channel, Response { swap_id, s_a: state3.s_a }).is_err() {
+
+                            let Ok(states) = self.db.get_states(swap_id).await else {
+                                tracing::error!(%swap_id, "Failed to read states from database");
+                                continue;
+                            };
+
+                            let s_a = states.iter().find_map(|state| {
+                                if let State::Alice(AliceState::BtcLocked { state3 }) = state {
+                                        Some(state3.s_a)
+                                } else {
+                                    None
+                                }
+                            });
+
+                            if let Err(_) = self.swarm.behaviour_mut().cooperative_xmr_redeem.send_response(channel, Response { swap_id, s_a: s_a.expect("Failed to get xmr key from database") }) {
                                 tracing::debug!(%peer, "Failed to respond with XMR key");
                                 continue;
                             }
