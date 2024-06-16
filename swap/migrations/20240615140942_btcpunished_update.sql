e71fd1f14c9955cf946a06f412f8b3d1df682948
patrini32 <171664803+patrini32@users.noreply.github.com>
2024-06-16 12:00:34 +0300

    better sql linting

--- swap/migrations/20240615140942_btcpunished_update.sql
+++ swap/migrations/20240615140942_btcpunished_update.sql
@@ -1,75 +1,128 @@
-UPDATE swap_states SET state = json_replace(
-    state, 
-    '$.Alice.Done', 
-    json_object(
-      'BtcPunished', 
-      (
-        SELECT json_extract ( state, '$.Alice.BtcLocked' )  FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Alice.BtcLocked' ) IS NOT NULL
-      )
-    )
-  ) 
-WHERE json_extract ( state, '$.Alice.Done' ) = 'BtcPunished';
-UPDATE swap_states SET state = json_replace (
-    state, 
-    '$.Bob', 
-    json_object(
-      'BtcPunished',
-      json_object (
-        'state',
-      json_insert (
-        (
-          SELECT json_extract ( state, '$.Bob.BtcCancelled' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Bob.BtcCancelled' ) IS NOT NULL
-        ),
-        '$.v', 
-        (
-          SELECT json_extract ( state, '$.Bob.BtcLocked.state3.v' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Bob.BtcLocked' ) IS NOT NULL
-        ), 
-        '$.monero_wallet_restore_blockheight', 
-        (
-          SELECT json_extract ( state, '$.Bob.BtcLocked.monero_wallet_restore_blockheight' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Bob.BtcLocked' ) IS NOT NULL
+UPDATE swap_states SET
+    state = json_replace(
+        state,
+        '$.Alice.Done',
+        json_object(
+            'BtcPunished',
+            (
+                SELECT json_extract(states.state, '$.Alice.BtcLocked')
+                FROM swap_states AS states
+                WHERE
+                    states.swap_id = swap_states.swap_id
+                    AND json_extract(
+                        states.state, '$.Alice.BtcLocked'
+                    ) IS NOT NULL
+            )
         )
-      ),
-    'tx_lock_id', 
-    json_extract (
-        state, '$.Bob.Done.BtcPunished.tx_lock_id'
     )
+WHERE json_extract(state, '$.Alice.Done') = 'BtcPunished';
+UPDATE swap_states SET
+    state = json_replace(
+        state,
+        '$.Bob',
+        json_object(
+            'BtcPunished',
+            json_object(
+                'state',
+                json_insert(
+                    (
+                        SELECT json_extract(states.state, '$.Bob.BtcCancelled')
+                        FROM swap_states AS states
+                        WHERE
+                            states.swap_id = swap_states.swap_id
+                            AND json_extract(
+                                states.state, '$.Bob.BtcCancelled'
+                            ) IS NOT NULL
+                    ),
+                    '$.v',
+                    (
+                        SELECT json_extract(states.state, '$.Bob.BtcLocked.state3.v')
+                        FROM swap_states AS states
+                        WHERE
+                            states.swap_id = swap_states.swap_id
+                            AND json_extract(
+                                states.state, '$.Bob.BtcLocked'
+                            ) IS NOT NULL
+                    ),
+                    '$.monero_wallet_restore_blockheight',
+                    (
+                        SELECT json_extract(states.state, '$.Bob.BtcLocked.monero_wallet_restore_blockheight')
+                        FROM swap_states AS states
+                        WHERE
+                            states.swap_id = swap_states.swap_id
+                            AND json_extract(
+                                states.state, '$.Bob.BtcLocked'
+                            ) IS NOT NULL
+                    )
+                ),
+                'tx_lock_id',
+                json_extract(
+                    state, '$.Bob.Done.BtcPunished.tx_lock_id'
+                )
+            )
+        )
     )
-  )
-  )
-WHERE json_extract( state, '$.Bob.Done.BtcPunished' ) IS NOT NULL;
-UPDATE swap_states SET state = json_insert (
-    state, 
-    '$.v', 
-    (
-      SELECT json_extract ( state, '$.Bob.BtcLocked.state3.v' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract( state, '$.Bob.BtcLocked' ) IS NOT NULL
-    ), 
-    '$.monero_wallet_restore_blockheight', 
-    (
-      SELECT json_extract ( state, '$.Bob.BtcLocked.monero_wallet_restore_blockheight' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Bob.BtcLocked' ) IS NOT NULL
+WHERE json_extract(state, '$.Bob.Done.BtcPunished') IS NOT NULL;
+UPDATE swap_states SET
+    state = json_insert(
+        state,
+        '$.v',
+        (
+            SELECT json_extract(states.state, '$.Bob.BtcLocked.state3.v')
+            FROM swap_states AS states
+            WHERE
+                states.swap_id = swap_states.swap_id
+                AND json_extract(states.state, '$.Bob.BtcLocked') IS NOT NULL
+        ),
+        '$.monero_wallet_restore_blockheight',
+        (
+            SELECT json_extract(states.state, '$.Bob.BtcLocked.monero_wallet_restore_blockheight')
+            FROM swap_states AS states
+            WHERE
+                states.swap_id = swap_states.swap_id
+                AND json_extract(states.state, '$.Bob.BtcLocked') IS NOT NULL
+        )
     )
-  ) 
 WHERE json_extract(state, '$.Bob.Done.BtcRefunded') IS NOT NULL;
-UPDATE swap_states SET state = json_insert (
-    state, 
-    '$.v', 
-    (
-      SELECT json_extract ( state, '$.Bob.BtcLocked.state3.v' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Bob.BtcLocked' ) IS NOT NULL
-    ), 
-    '$.monero_wallet_restore_blockheight', 
-    (
-      SELECT json_extract ( state, '$.Bob.BtcLocked.monero_wallet_restore_blockheight' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Bob.BtcLocked' ) IS NOT NULL
+UPDATE swap_states SET
+    state = json_insert(
+        state,
+        '$.v',
+        (
+            SELECT json_extract(states.state, '$.Bob.BtcLocked.state3.v')
+            FROM swap_states AS states
+            WHERE
+                states.swap_id = swap_states.swap_id
+                AND json_extract(states.state, '$.Bob.BtcLocked') IS NOT NULL
+        ),
+        '$.monero_wallet_restore_blockheight',
+        (
+            SELECT json_extract(states.state, '$.Bob.BtcLocked.monero_wallet_restore_blockheight')
+            FROM swap_states AS states
+            WHERE
+                states.swap_id = swap_states.swap_id
+                AND json_extract(states.state, '$.Bob.BtcLocked') IS NOT NULL
+        )
     )
-  ) 
-WHERE json_extract( state, '$.Bob.BtcCancelled' ) IS NOT NULL;
-UPDATE swap_states SET state = json_insert(
-    state, 
-    '$.v', 
-    (
-      SELECT json_extract ( state, '$.Bob.BtcLocked.state3.v' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Bob.BtcLocked' ) IS NOT NULL
-    ), 
-    '$.monero_wallet_restore_blockheight', 
-    (
-      SELECT json_extract ( state, '$.Bob.BtcLocked.monero_wallet_restore_blockheight' ) FROM swap_states states WHERE states.swap_id = swap_states.swap_id AND json_extract ( state, '$.Bob.BtcLocked' ) IS NOT NULL
+WHERE json_extract(state, '$.Bob.BtcCancelled') IS NOT NULL;
+UPDATE swap_states SET
+    state = json_insert(
+        state,
+        '$.v',
+        (
+            SELECT json_extract(states.state, '$.Bob.BtcLocked.state3.v')
+            FROM swap_states AS states
+            WHERE
+                states.swap_id = swap_states.swap_id
+                AND json_extract(states.state, '$.Bob.BtcLocked') IS NOT NULL
+        ),
+        '$.monero_wallet_restore_blockheight',
+        (
+            SELECT json_extract(states.state, '$.Bob.BtcLocked.monero_wallet_restore_blockheight')
+            FROM swap_states AS states
+            WHERE
+                states.swap_id = swap_states.swap_id
+                AND json_extract(states.state, '$.Bob.BtcLocked') IS NOT NULL
+        )
     )
-  ) 
-WHERE json_extract ( state, '$.Bob.CancelTimelockExpired' ) IS NOT NULL;
+WHERE json_extract(state, '$.Bob.CancelTimelockExpired') IS NOT NULL;
