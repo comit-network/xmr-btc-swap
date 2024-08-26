fea1e66c642ec1d8109c620942fbf4ee673815d3
binarybaron <binarybaron@unstoppableswap.net>
2024-08-26 15:04:22 +0200

    chore(gui): Upgrade @tauri-apps/api and add eslint

---
+++ src-gui/eslint.config.js
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
