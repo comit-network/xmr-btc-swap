f4ada78929acff09b884a6a437ac398ba2f0030f
Einliterflasche <einliterflasche@pm.me>
2024-08-28 12:25:30 +0200

    refactor(cli): introduce tauri feature flag to allow the project to be compiled without tauri

--- swap/src/cli/api/tauri_bindings.rs
+++ swap/src/cli/api/tauri_bindings.rs
@@ -4,8 +4,6 @@
 use anyhow::Result;
 use bitcoin::Txid;
 use serde::Serialize;
-use std::sync::Arc;
-use tauri::{AppHandle, Emitter};
 use typeshare::typeshare;
 use uuid::Uuid;
 
@@ -14,15 +12,27 @@ use crate::{monero, network::quote::BidQuote};
 static SWAP_PROGRESS_EVENT_NAME: &str = "swap-progress-update";
 
 #[derive(Debug, Clone)]
-pub struct TauriHandle(Arc<AppHandle>);
+pub struct TauriHandle(
+    #[cfg(feature = "tauri")]
+    #[cfg_attr(feature = "tauri", allow(unused))]
+    std::sync::Arc<tauri::AppHandle>,
+);
 
 impl TauriHandle {
-    pub fn new(tauri_handle: AppHandle) -> Self {
-        Self(Arc::new(tauri_handle))
+    #[cfg(feature = "tauri")]
+    pub fn new(tauri_handle: tauri::AppHandle) -> Self {
+        Self(
+            #[cfg(feature = "tauri")]
+            std::sync::Arc::new(tauri_handle),
+        )
     }
 
+    #[allow(unused_variables)]
     pub fn emit_tauri_event<S: Serialize + Clone>(&self, event: &str, payload: S) -> Result<()> {
-        self.0.emit(event, payload).map_err(|e| e.into())
+        #[cfg(tauri)]
+        self.0.emit(event, payload).map_err(|e| e.into())?;
+
+        Ok(())
     }
 }
 
