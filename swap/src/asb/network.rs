comit-network/xmr-btc-swap

commit d53c12d64e6ae9241c79ec495c5f327a74c2d2cc

feat(asb, cli): Listen on onion address, dial onion addresses (#203)
This pull requests
- Adds rust native support for the `asb` to listen on an onion service. Previously we were depedent on a seperately running `torc` client. Instead we now use [arti](https://tpo.pages.torproject.net/core/arti/), a rust implementation of the tor protocol.
- Removes the `tor.control_port` and `tor.socks5_port` property from the config of the `asb`
- Adds a new `tor.register_hidden_service` boolean property to the config of the `asb` which when enabled automatically runs a hidden service at startup
- Adds a new `tor.hidden_service_num_intro_points` config property to specify how many introduction points to register the onion service at
- Adds support for the `cli` to dial onion addresses

This is dependent on https://github.com/umgefahren/libp2p-tor/pull/24

Closes https://github.com/UnstoppableSwap/core/issues/16

swap/src/asb/network.rs
@@ -22,16 +22,81 @@ use std::time::Duration;
 use uuid::Uuid;
 
 pub mod transport {
-    use libp2p::{dns, identity, tcp, Transport};
+    use std::sync::Arc;
+
+    use arti_client::{config::onion_service::OnionServiceConfigBuilder, TorClient};
+    use libp2p::{core::transport::OptionalTransport, dns, identity, tcp, Transport};
+    use libp2p_community_tor::AddressConversion;
+    use tor_rtcompat::tokio::TokioRustlsRuntime;
 
     use super::*;
 
+    static ASB_ONION_SERVICE_NICKNAME: &str = "asb";
+    static ASB_ONION_SERVICE_PORT: u16 = 9939;
+
+    type OnionTransportWithAddresses = (Boxed<(PeerId, StreamMuxerBox)>, Vec<Multiaddr>);
+
     /// Creates the libp2p transport for the ASB.
-    pub fn new(identity: &identity::Keypair) -> Result<Boxed<(PeerId, StreamMuxerBox)>> {
-        let tcp = tcp::tokio::Transport::new(tcp::Config::new().nodelay(true));
+    ///
+    /// If you pass in a `None` for `maybe_tor_client`, the ASB will not use Tor at all.
+    ///
+    /// If you pass in a `Some(tor_client)`, the ASB will listen on an onion service and return
+    /// the onion address. If it fails to listen on the onion address, it will only use tor for
+    /// dialing and not listening.
+    pub fn new(
+        identity: &identity::Keypair,
+        maybe_tor_client: Option<Arc<TorClient<TokioRustlsRuntime>>>,
+        num_intro_points: u8,
+        register_hidden_service: bool,
+    ) -> Result<OnionTransportWithAddresses> {
+        let (maybe_tor_transport, onion_addresses) = if let Some(tor_client) = maybe_tor_client {
+            let mut tor_transport = libp2p_community_tor::TorTransport::from_client(
+                tor_client,
+                AddressConversion::DnsOnly,
+            );
+
+            let addresses = if register_hidden_service {
+                let onion_service_config = OnionServiceConfigBuilder::default()
+                    .nickname(
+                        ASB_ONION_SERVICE_NICKNAME
+                            .parse()
+                            .expect("Static nickname to be valid"),
+                    )
+                    .num_intro_points(num_intro_points)
+                    .build()
+                    .expect("We specified a valid nickname");
+
+                match tor_transport.add_onion_service(onion_service_config, ASB_ONION_SERVICE_PORT)
+                {
+                    Ok(addr) => {
+                        tracing::debug!(
+                            %addr,
+                            "Setting up onion service for libp2p to listen on"
+                        );
+                        vec![addr]
+                    }
+                    Err(err) => {
+                        tracing::warn!(error=%err, "Failed to listen on onion address");
+                        vec![]
+                    }
+                }
+            } else {
+                vec![]
+            };
+
+            (OptionalTransport::some(tor_transport), addresses)
+        } else {
+            (OptionalTransport::none(), vec![])
+        };
+
+        let tcp = maybe_tor_transport
+            .or_transport(tcp::tokio::Transport::new(tcp::Config::new().nodelay(true)));
         let tcp_with_dns = dns::tokio::Transport::system(tcp)?;
 
-        authenticate_and_multiplex(tcp_with_dns.boxed(), identity)
+        Ok((
+            authenticate_and_multiplex(tcp_with_dns.boxed(), identity)?,
+            onion_addresses,
+        ))
     }
 }
 

swap/src/bin/asb.rs
@@ -14,12 +14,9 @@
 
 use anyhow::{bail, Context, Result};
 use comfy_table::Table;
-use libp2p::core::multiaddr::Protocol;
-use libp2p::core::Multiaddr;
 use libp2p::Swarm;
 use std::convert::TryInto;
 use std::env;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::sync::Arc;
 use structopt::clap;
 use structopt::clap::ErrorKind;
@@ -28,6 +25,7 @@ use swap::asb::config::{
     initial_setup, query_user_for_initial_config, read_config, Config, ConfigNotInitialized,
 };
 use swap::asb::{cancel, punish, redeem, refund, safely_abort, EventLoop, Finality, KrakenRate};
+use swap::common::tor::init_tor_client;
 use swap::common::tracing_util::Format;
 use swap::common::{self, get_logs, warn_if_outdated};
 use swap::database::{open_db, AccessMode};
@@ -35,8 +33,7 @@ use swap::network::rendezvous::XmrBtcNamespace;
 use swap::network::swarm;
 use swap::protocol::alice::{run, AliceState};
 use swap::seed::Seed;
-use swap::tor::AuthenticatedClient;
-use swap::{bitcoin, kraken, monero, tor};
+use swap::{bitcoin, kraken, monero};
 use tracing_subscriber::filter::LevelFilter;
 
 const DEFAULT_WALLET_NAME: &str = "asb-wallet";
@@ -149,29 +146,16 @@ pub async fn main() -> Result<()> {
             let bitcoin_balance = bitcoin_wallet.balance().await?;
             tracing::info!(%bitcoin_balance, "Bitcoin wallet balance");
 
+            // Connect to Kraken
             let kraken_price_updates = kraken::connect(config.maker.price_ticker_ws_url.clone())?;
-
-            // Setup Tor hidden services
-            let tor_client =
-                tor::Client::new(config.tor.socks5_port).with_control_port(config.tor.control_port);
-            let _ac = match tor_client.assert_tor_running().await {
-                Ok(_) => {
-                    tracing::info!("Setting up Tor hidden service");
-                    let ac =
-                        register_tor_services(config.network.clone().listen, tor_client, &seed)
-                            .await?;
-                    Some(ac)
-                }
-                Err(_) => {
-                    tracing::warn!("Tor not found. Running on clear net");
-                    None
-                }
-            };
 
             let kraken_rate = KrakenRate::new(config.maker.ask_spread, kraken_price_updates);
             let namespace = XmrBtcNamespace::from_is_testnet(testnet);
 
-            let mut swarm = swarm::asb(
+            // Initialize Tor client
+            let tor_client = init_tor_client(&config.data.dir).await?.into();
+
+            let (mut swarm, onion_addresses) = swarm::asb(
                 &seed,
                 config.maker.min_buy_btc,
                 config.maker.max_buy_btc,
@@ -180,6 +164,8 @@ pub async fn main() -> Result<()> {
                 env_config,
                 namespace,
                 &rendezvous_addrs,
+                tor_client,
+                config.tor,
             )?;
 
             for listen in config.network.listen.clone() {
@@ -188,10 +174,25 @@ pub async fn main() -> Result<()> {
                 }
             }
 
+            for onion_address in onion_addresses {
+                match swarm.listen_on(onion_address.clone()) {
+                    Err(e) => {
+                        tracing::warn!(
+                            "Failed to listen on onion address {}: {}",
+                            onion_address,
+                            e
+                        );
+                    }
+                    _ => {
+                        swarm.add_external_address(onion_address);
+                    }
+                }
+            }
+
             tracing::info!(peer_id = %swarm.local_peer_id(), "Network layer initialized");
 
             for external_address in config.network.external_addresses {
-                Swarm::add_external_address(&mut swarm, external_address);
+                swarm.add_external_address(external_address);
             }
 
             let (event_loop, mut swap_receiver) = EventLoop::new(
@@ -390,47 +391,4 @@ async fn init_monero_wallet(
     .await?;
 
     Ok(wallet)
-}
-
-/// Registers a hidden service for each network.
-/// Note: Once ac goes out of scope, the services will be de-registered.
-async fn register_tor_services(
-    networks: Vec<Multiaddr>,
-    tor_client: tor::Client,
-    seed: &Seed,
-) -> Result<AuthenticatedClient> {
-    let mut ac = tor_client.into_authenticated_client().await?;
-
-    let hidden_services_details = networks
-        .iter()
-        .flat_map(|network| {
-            network.iter().map(|protocol| match protocol {
-                Protocol::Tcp(port) => Some((
-                    port,
-                    SocketAddr::new(IpAddr::from(Ipv4Addr::new(127, 0, 0, 1)), port),
-                )),
-                _ => {
-                    // We only care for Tcp for now.
-                    None
-                }
-            })
-        })
-        .flatten()
-        .collect::<Vec<_>>();
-
-    let key = seed.derive_torv3_key();
-
-    ac.add_services(&hidden_services_details, &key).await?;
-
-    let onion_address = key
-        .public()
-        .get_onion_address()
-        .get_address_without_dot_onion();
-
-    hidden_services_details.iter().for_each(|(port, _)| {
-        let onion_address = format!("/onion3/{}:{}", onion_address, port);
-        tracing::info!(%onion_address, "Successfully created hidden service");
-    });
-
-    Ok(ac)
 }

swap/src/network/swarm.rs
@@ -1,3 +1,4 @@
+use crate::asb::config::TorConf;
 use crate::asb::{LatestRate, RendezvousNode};
 use crate::libp2p_ext::MultiAddrExt;
 use crate::network::rendezvous::XmrBtcNamespace;
@@ -23,7 +24,9 @@ pub fn asb<LR>(
     env_config: env::Config,
     namespace: XmrBtcNamespace,
     rendezvous_addrs: &[Multiaddr],
-) -> Result<Swarm<asb::Behaviour<LR>>>
+    maybe_tor_client: Option<Arc<TorClient<TokioRustlsRuntime>>>,
+    tor_conf: TorConf,
+) -> Result<(Swarm<asb::Behaviour<LR>>, Vec<Multiaddr>)>
 where
     LR: LatestRate + Send + 'static + Debug + Clone,
 {
@@ -50,7 +53,12 @@ where
         rendezvous_nodes,
     );
 
-    let transport = asb::transport::new(&identity)?;
+    let (transport, onion_addresses) = asb::transport::new(
+        &identity,
+        maybe_tor_client,
+        tor_conf.hidden_service_num_intro_points,
+        tor_conf.register_hidden_service,
+    )?;
 
     let swarm = SwarmBuilder::with_existing_identity(identity)
         .with_tokio()
@@ -59,7 +67,7 @@ where
         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::MAX))
         .build();
 
-    Ok(swarm)
+    Ok((swarm, onion_addresses))
 }
 
 pub async fn cli<T>(

swap/tests/harness/mod.rs
@@ -243,7 +243,7 @@ async fn start_alice(
     let latest_rate = FixedRate::default();
     let resume_only = false;
 
-    let mut swarm = swarm::asb(
+    let (mut swarm, _) = swarm::asb(
         seed,
         min_buy,
         max_buy,
@@ -252,6 +252,7 @@ async fn start_alice(
         env_config,
         XmrBtcNamespace::Testnet,
         &[],
+        None,
     )
     .unwrap();
     swarm.listen_on(listen_address).unwrap();

swap/src/common/mod.rs
@@ -1,3 +1,4 @@
+pub mod tor;
 pub mod tracing_util;
 
 use anyhow::anyhow;

src-tauri/src/lib.rs
@@ -275,8 +275,14 @@ async fn initialize_context(
             .to_string_lossy()
             .starts_with("monero-wallet-rpc")
         {
-            println!("Killing monero-wallet-rpc process with pid: {}", pid);
-            process.kill();
+            #[cfg(not(debug_assertions))]
+            {
+                println!("Killing monero-wallet-rpc process with pid: {}", pid);
+                process.kill();
+            }
+
+            #[cfg(debug_assertions)]
+            println!("Would kill monero-wallet-rpc process with pid: {}", pid);
         }
     }
 

swap/Cargo.toml
@@ -42,7 +42,7 @@ hex = "0.4"
 jsonrpsee = { version = "0.16.2", features = [ "server" ] }
 jsonrpsee-core = "0.16.2"
 libp2p = { version = "0.53.2", features = [ "tcp", "yamux", "dns", "noise", "request-response", "ping", "rendezvous", "identify", "macros", "cbor", "json", "tokio", "serde", "rsa" ] }
-libp2p-community-tor = { git = "https://github.com/UnstoppableSwap/libp2p-tor", branch = "main" }
+libp2p-community-tor = { git = "https://github.com/UnstoppableSwap/libp2p-tor", branch = "fix/announce-address-immed", features = [ "listen-onion-service" ] }
 monero = { version = "0.12", features = [ "serde_support" ] }
 monero-rpc = { path = "../monero-rpc" }
 once_cell = "1.19"
@@ -96,10 +96,6 @@ tokio-tungstenite = { version = "0.15", features = [ "rustls-tls" ] }
 tokio-util = { version = "0.7", features = [ "io", "codec" ] }
 toml = "0.8"
 tor-rtcompat = "0.24.0"
-torut = { version = "0.2", default-features = false, features = [
-    "v3",
-    "control",
-] }
 tower = { version = "0.4.13", features = [ "full" ] }
 tower-http = { version = "0.3.4", features = [ "full" ] }
 tracing = { version = "0.1", features = [ "attributes" ] }

swap/src/asb/config.rs
@@ -1,10 +1,9 @@
 use crate::env::{Mainnet, Testnet};
 use crate::fs::{ensure_directory_exists, system_config_dir, system_data_dir};
-use crate::tor::{DEFAULT_CONTROL_PORT, DEFAULT_SOCKS5_PORT};
 use anyhow::{bail, Context, Result};
 use config::ConfigError;
 use dialoguer::theme::ColorfulTheme;
-use dialoguer::Input;
+use dialoguer::{Input, Select};
 use libp2p::core::Multiaddr;
 use rust_decimal::prelude::FromPrimitive;
 use rust_decimal::Decimal;
@@ -200,8 +199,8 @@ pub struct Monero {
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct TorConf {
-    pub control_port: u16,
-    pub socks5_port: u16,
+    pub register_hidden_service: bool,
+    pub hidden_service_num_intro_points: u8,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
@@ -219,8 +218,8 @@ pub struct Maker {
 impl Default for TorConf {
     fn default() -> Self {
         Self {
-            control_port: DEFAULT_CONTROL_PORT,
-            socks5_port: DEFAULT_SOCKS5_PORT,
+            register_hidden_service: true,
+            hidden_service_num_intro_points: 5,
         }
     }
 }
@@ -313,15 +312,12 @@ pub fn query_user_for_initial_config(testnet: bool) -> Result<Config> {
         .default(defaults.monero_wallet_rpc_url)
         .interact_text()?;
 
-    let tor_control_port = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("Enter Tor control port or hit enter to use default. If Tor is not running on your machine, no hidden service will be created.")
-        .default(DEFAULT_CONTROL_PORT.to_owned())
-        .interact_text()?;
-
-    let tor_socks5_port = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("Enter Tor socks5 port or hit enter to use default")
-        .default(DEFAULT_SOCKS5_PORT.to_owned())
-        .interact_text()?;
+    let register_hidden_service = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Do you want a Tor hidden service to be created? This will allow you to run from behind a firewall without opening ports, and hide your IP address. You do not have to run a Tor daemon yourself. We recommend this for most users. (y/n)")
+        .items(&["yes", "no"])
+        .default(0)
+        .interact()?
+        == 0;
 
     let min_buy = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Enter minimum Bitcoin amount you are willing to accept per swap or hit enter to use default.")
@@ -387,8 +383,8 @@ pub fn query_user_for_initial_config(testnet: bool) -> Result<Config> {
             network: monero_network,
         },
         tor: TorConf {
-            control_port: tor_control_port,
-            socks5_port: tor_socks5_port,
+            register_hidden_service,
+            ..Default::default()
         },
         maker: Maker {
             min_buy_btc: min_buy,

commit 2633e8e6bf58c25d141b617966f57b76c01bc896

fix(asb): Compilation error introduced in #203 (#222)

swap/src/asb/network.rs
@@ -46,8 +46,8 @@ pub mod transport {
     pub fn new(
         identity: &identity::Keypair,
         maybe_tor_client: Option<Arc<TorClient<TokioRustlsRuntime>>>,
-        num_intro_points: u8,
         register_hidden_service: bool,
+        num_intro_points: u8,
     ) -> Result<OnionTransportWithAddresses> {
         let (maybe_tor_transport, onion_addresses) = if let Some(tor_client) = maybe_tor_client {
             let mut tor_transport = libp2p_community_tor::TorTransport::from_client(

swap/src/bin/asb.rs
@@ -165,7 +165,8 @@ pub async fn main() -> Result<()> {
                 namespace,
                 &rendezvous_addrs,
                 tor_client,
-                config.tor,
+                config.tor.register_hidden_service,
+                config.tor.hidden_service_num_intro_points,
             )?;
 
             for listen in config.network.listen.clone() {

swap/src/network/swarm.rs
@@ -1,4 +1,3 @@
-use crate::asb::config::TorConf;
 use crate::asb::{LatestRate, RendezvousNode};
 use crate::libp2p_ext::MultiAddrExt;
 use crate::network::rendezvous::XmrBtcNamespace;
@@ -25,7 +24,8 @@ pub fn asb<LR>(
     namespace: XmrBtcNamespace,
     rendezvous_addrs: &[Multiaddr],
     maybe_tor_client: Option<Arc<TorClient<TokioRustlsRuntime>>>,
-    tor_conf: TorConf,
+    register_hidden_service: bool,
+    num_intro_points: u8,
 ) -> Result<(Swarm<asb::Behaviour<LR>>, Vec<Multiaddr>)>
 where
     LR: LatestRate + Send + 'static + Debug + Clone,
@@ -56,8 +56,8 @@ where
     let (transport, onion_addresses) = asb::transport::new(
         &identity,
         maybe_tor_client,
-        tor_conf.hidden_service_num_intro_points,
-        tor_conf.register_hidden_service,
+        register_hidden_service,
+        num_intro_points,
     )?;
 
     let swarm = SwarmBuilder::with_existing_identity(identity)

swap/tests/harness/mod.rs
@@ -253,6 +253,8 @@ async fn start_alice(
         XmrBtcNamespace::Testnet,
         &[],
         None,
+        false,
+        1,
     )
     .unwrap();
     swarm.listen_on(listen_address).unwrap();